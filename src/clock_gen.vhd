ece0ff2f0cc6fe6fe338055ea3e5ed9295839552
Joshua Bassett <hello@joshbassett.info>
2019-07-06 20:23:03 +1000

    Refactor clock generator

--- src/clock_gen.vhd
+++ src/clock_gen.vhd
@@ -15,26 +15,27 @@ entity clock_gen is
 end clock_gen;
 
 architecture arch of clock_gen is
-  signal even_count : unsigned(0 downto 0);
-  signal odd_count : unsigned(1 downto 0);
 begin
   process(clk_12)
+    variable even_count : integer range 0 to 1;
+    variable odd_count : integer range 0 to 2;
   begin
     if rising_edge(clk_12) then
       if even_count < 1 then
-        even_count <= even_count + 1;
+        even_count := even_count + 1;
+        cen_6 <= '0';
       else
-        even_count <= (others => '0');
+        even_count := 0;
+        cen_6 <= '1';
       end if;
 
       if odd_count < 2 then
-        odd_count <= odd_count + 1;
+        odd_count := odd_count + 1;
+        cen_4 <= '0';
       else
-        odd_count <= (others => '0');
+        odd_count := 0;
+        cen_4 <= '1';
       end if;
     end if;
   end process;
-
-  cen_6 <= '1' when even_count = 1 else '0';
-  cen_4 <= '1' when odd_count = 2 else '0';
 end architecture;
